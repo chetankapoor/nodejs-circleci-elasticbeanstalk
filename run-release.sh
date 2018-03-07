5feb2c1a9a8e398e4cf6ba79e0378ca7fab41384
Chetan Kapoor <chetankapoor321@gmail.com>
2018-03-07 15:12:58 +0100

    Circle

--- run-release.sh
+++ run-release.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-eb use test-ck --profile lumen --region us-east-2
-eb deploy -v --staged --profile lumen --region us-east-2
+eb use test-ck --profile lumen --region us-east-2 && eb deploy -v --staged --profile lumen --region us-east-2
